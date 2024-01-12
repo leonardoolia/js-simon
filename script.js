bc8311c862ddb62a63ad76de0ed66619c8debc86
leonardoolia <leonardooliaa@gmail.com>
2024-01-12 16:36:34 +0100

    created function to generate 5 random numbers

--- script.js
+++ script.js
@@ -12,8 +12,12 @@ const screenTimer = document.getElementById('timer');
 const screenText = document.getElementById('testo-numeri');
 const screenNumbers = document.getElementById('numeri');
 
+// Range numeri casuali
+const min = 1;
+const max = 99;
 
-
+// Numero di celle(numeri) in pagina
+const cells = 5;
 
 //? -----------------------------------------------------------------------
 //? FUNZIONI GLOBALI
@@ -26,9 +30,49 @@ const startGame = () => {
     startButton.classList.add('d-none');
     gameSection.classList.remove('d-none');
 
+    // -----------------------------------------------------------------------
+    // Funzioni interne
+    // -----------------------------------------------------------------------
+
+    // Funzione per generare 5 numeri casuali
+    const generateRandomNumbers = (min, max) => {
+        const listOfNumbers = [];
+
+        do {
+            const randomNumber = Math.floor(Math.random() * (max + 1 - min)) + min;
+            if (!listOfNumbers.includes(randomNumber)) listOfNumbers.push(randomNumber);
+
+        } while (
+            listOfNumbers.length < cells
+        )
+
+        console.log(listOfNumbers)
+
+    }
 
 
 
+
+    // TIMER
+    let secondsLeft = 5;
+    screenTimer.innerText = secondsLeft;
+
+    const timer = setInterval(() => {
+        screenTimer.innerText = --secondsLeft;
+
+        // Quando il timer arriva a 0, interrompiamo il countdown
+        if (secondsLeft === 0) {
+            clearInterval(timer);
+            screenTimer.innerText = 'VIA!';
+        };
+
+    }, 1000);
+
+
+
+
+    // GENERO 5 NUMERI CASUALI DA 1 A 99
+    const randomNumbers = generateRandomNumbers(min, max);
 
 
 
