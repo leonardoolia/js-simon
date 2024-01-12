660d0ab05d8292aad163f42c687db8184f2429aa
leonardoolia <leonardooliaa@gmail.com>
2024-01-12 15:20:31 +0100

    created functions to start or exit the game

--- script.js
+++ script.js
@@ -13,3 +13,52 @@ const screenText = document.getElementById('testo-numeri');
 const screenNumbers = document.getElementById('numeri');
 
 
+
+
+//? -----------------------------------------------------------------------
+//? FUNZIONI GLOBALI
+//? -----------------------------------------------------------------------
+
+//? FUNZIONE PER INIZIARE IL GIOCO
+const startGame = () => {
+
+    // Al click scompare il bottone e inizia il gioco
+    startButton.classList.add('d-none');
+    gameSection.classList.remove('d-none');
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//? FUNZIONE PER USCIRE DAL GIOCO
+const exitGame = () => {
+    startButton.classList.remove('d-none');
+    gameSection.classList.add('d-none');
+}
+
+
+
+
+
+
+
+//? CLICK DEL BOTTONE E INIZIO DEL GIOCO
+startButton.addEventListener('click', startGame);
+
+
+//? CLICK DEL BOTTONE PER USCIRE DAL GIOCO
+startAgainButton.addEventListener('click', exitGame);
