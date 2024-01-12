9d324f04ad0d2a3b92c5d8006413b8f3c2c134c0
leonardoolia <leonardooliaa@gmail.com>
2024-01-12 15:19:14 +0100

    got dom elements

---
+++ script.js
@@ -0,0 +1,15 @@
+//? -----------------------------------------------------------------------
+//? RECUPERO ELEMENTI DAL DOM
+//? -----------------------------------------------------------------------
+
+// Bottoni
+const startButton = document.getElementById('start-button');
+const startAgainButton = document.getElementById('play-again-button')
+
+// Sezione con timer e numeri
+const gameSection = document.getElementById('sezione-gioco');
+const screenTimer = document.getElementById('timer');
+const screenText = document.getElementById('testo-numeri');
+const screenNumbers = document.getElementById('numeri');
+
+
